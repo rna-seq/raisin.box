3a830ed8c358197bad1fdb0023682c25a3a237d9
Maik R��der <maikroeder@gmail.com>
2011-07-09 12:16:36 +0000

    =add docstring

--- raisin/box/__init__.py
+++ raisin/box/__init__.py
@@ -1,3 +1,25 @@
+"""Provide information about boxes and the associated resources
+
+RESOURCES: Read from the resources.ini file
+
+    * Used by raisin.restyler to fetch the resource
+
+BOXES: Read from the boxes.ini file
+
+    * Used by raisin.restyler to verify that a box id passed in a url really exists
+
+    * Used by raisin.restyler to get meta information about the boxes
+
+resources_registry: A list of registered resources. For each resource it contains
+
+    * the name of the resource
+    
+    * the method to call for augmenting the box information obtained from BOXES
+    
+    * the list of formats that the augmentation method needs
+    
+"""
+
 import os
 from configobj import ConfigObj
 
