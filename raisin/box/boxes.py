4c0f36222f5c546c1c6f3c8005b10be0380433b2
Maik R��der <maikroeder@gmail.com>
2011-07-11 11:46:42 +0000

    =rename augment

--- raisin/box/boxes.py
+++ raisin/box/boxes.py
@@ -12,12 +12,12 @@ from raisin.box import RESOURCES_REGISTRY
 from gvizapi import gviz_api
 
 
-class augment_resource(object):
+class augment(object):
     """This is a decorator that registers methods augmenting resources.
 
     Use the decorator like this:
 
-        @augment_resource((JSON,PICKLED))
+        @augment((JSON,PICKLED))
         def project_meta(self, box):
 
     The list of formats needed in the method are given as the formats parameter.
@@ -56,7 +56,7 @@ def get_lines(box):
                           skipinitialspace=True)
 
 
-@augment_resource((PICKLED,))
+@augment((PICKLED,))
 def projects(self, box):
     lines = get_lines(box)
     box['title'] = ''
@@ -65,7 +65,7 @@ def projects(self, box):
     return box
 
 
-@augment_resource((PICKLED,))
+@augment((PICKLED,))
 def project_about(self, box):
     lines = get_lines(box)
     line = {}
@@ -79,7 +79,7 @@ def project_about(self, box):
     return box
 
 
-@augment_resource((PICKLED,))
+@augment((PICKLED,))
 def run_about(self, box):
     lines = get_lines(box)
     line = {}
@@ -93,7 +93,7 @@ def run_about(self, box):
     return box
 
 
-@augment_resource((PICKLED,))
+@augment((PICKLED,))
 def project_meta(self, box):
     lines = get_lines(box)
     line = {}
@@ -107,7 +107,7 @@ def project_meta(self, box):
     return box
 
 
-@augment_resource((PICKLED,))
+@augment((PICKLED,))
 def experiment_about(self, box):
     box['title'] = 'About'
     lines = get_lines(box)
@@ -121,7 +121,7 @@ def experiment_about(self, box):
     return box
 
 
-@augment_resource((PICKLED,))
+@augment((PICKLED,))
 def experiments(self, box):
     lines = get_lines(box)
     box['title'] = ''
@@ -130,7 +130,7 @@ def experiments(self, box):
     return box
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def project_experimentstable(self, box):
     column_number = len(box[PICKLED]['table_description'])
     js = """
@@ -146,7 +146,7 @@ def project_experimentstable(self, box):
     return box
 
 
-@augment_resource((JSON, PICKLED,))
+@augment((JSON, PICKLED,))
 def project_experiment_subset_selection(self, box):
     js = """
    function makeExperimentSubsetLink(dataTable, rowNum){
@@ -158,7 +158,7 @@ def project_experiment_subset_selection(self, box):
     return box
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def project_experiment_subset(self, box):
     column_number = len(box[PICKLED]['table_description'])
     js = """
@@ -174,7 +174,7 @@ def project_experiment_subset(self, box):
     return box
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def project_downloads(self, box):
     js = """
    function makeDownloadLink(dataTable, rowNum){
@@ -186,23 +186,23 @@ def project_downloads(self, box):
     return box
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def rnadashboard(self, box):
     box['title'] = ''
     return box
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def rnadashboard_results(self, box):
     return box
 
 
-@augment_resource((PICKLED,))
+@augment((PICKLED,))
 def experiment_sample_info(self, box):
     return _sample_info(self, box)
 
 
-@augment_resource((PICKLED,))
+@augment((PICKLED,))
 def run_sample_info(self, box):
     return _sample_info(self, box)
 
@@ -233,17 +233,17 @@ def _sample_info(self, box):
     return box
 
 
-@augment_resource((PICKLED,))
+@augment((PICKLED,))
 def experiment_mapping_info(self, box):
     return _mapping_info(self, box)
 
 
-@augment_resource((PICKLED,))
+@augment((PICKLED,))
 def run_mapping_info(self, box):
     return _mapping_info(self, box)
 
 
-@augment_resource((PICKLED,))
+@augment((PICKLED,))
 def lane_mapping_info(self, box):
     return _mapping_info(self, box)
 
@@ -282,377 +282,377 @@ def _mapping_info(self, box):
     return box
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def experiment_read_summary(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def run_read_summary(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def lane_read_summary(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def experiment_mapping_summary(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def run_mapping_summary(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def lane_mapping_summary(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def experiment_expression_summary(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def run_expression_summary(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def lane_expression_summary(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def experiment_splicing_summary(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def run_splicing_summary(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def lane_splicing_summary(self, box):
     pass
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_reads_containing_ambiguous_nucleotides(self, box):
     return _custom_spaced_chart(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_reads_containing_ambiguous_nucleotides(self, box):
     return _custom_spaced_chart(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_reads_containing_only_unambiguous_nucleotides(self, box):
     return _custom_spaced_chart(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_reads_containing_only_unambiguous_nucleotides(self, box):
     return _custom_spaced_chart(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_average_percentage_of_unique_reads(self, box):
     return _custom_spaced_chart(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_average_percentage_of_unique_reads(self, box):
     return _custom_spaced_chart(self, box)
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def experiment_total_ambiguous_and_unambiguous_reads(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def run_total_ambiguous_and_unambiguous_reads(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def lane_total_ambiguous_and_unambiguous_reads(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def experiment_average_and_average_unique_reads(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def run_average_and_average_unique_reads(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def lane_average_and_average_unique_reads(self, box):
     pass
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_percentage_of_reads_with_ambiguous_bases(self, box):
     return _percentage_of_reads_with_ambiguous_bases(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_percentage_of_reads_with_ambiguous_bases(self, box):
     return _percentage_of_reads_with_ambiguous_bases(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def lane_percentage_of_reads_with_ambiguous_bases(self, box):
     return _percentage_of_reads_with_ambiguous_bases(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_quality_score_by_position(self, box):
     return _position(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_quality_score_by_position(self, box):
     return _position(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def lane_quality_score_by_position(self, box):
     return _position(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_ambiguous_bases_per_position(self, box):
     return _position(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_ambiguous_bases_per_position(self, box):
     return _position(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def lane_ambiguous_bases_per_position(self, box):
     return _position(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_merged_mapped_reads(self, box):
     return _mapped_reads(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_merged_mapped_reads(self, box):
     return _mapped_reads(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def lane_merged_mapped_reads(self, box):
     return _mapped_reads(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_genome_mapped_reads(self, box):
     return _mapped_reads(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_genome_mapped_reads(self, box):
     return _mapped_reads(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def lane_genome_mapped_reads(self, box):
     return _mapped_reads(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_junction_mapped_reads(self, box):
     return _mapped_reads(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_junction_mapped_reads(self, box):
     return _mapped_reads(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def lane_junction_mapped_reads(self, box):
     return _mapped_reads(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_split_mapped_reads(self, box):
     return _mapped_reads(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_split_mapped_reads(self, box):
     return _mapped_reads(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def lane_split_mapped_reads(self, box):
     return _mapped_reads(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_detected_genes(self, box):
     return _detected_genes(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_detected_genes(self, box):
     return _detected_genes(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def lane_detected_genes(self, box):
     return _detected_genes(self, box)
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def experiment_gene_expression_profile(self, box):
     return _gene_expression_profile(self, box)
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def run_gene_expression_profile(self, box):
     return _gene_expression_profile(self, box)
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def lane_gene_expression_profile(self, box):
     return _gene_expression_profile(self, box)
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def experiment_gene_expression_levels(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def run_gene_expression_levels(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def lane_gene_expression_levels(self, box):
     pass
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_top_genes(self, box):
     return _thousands_formatter(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_top_genes(self, box):
     return _thousands_formatter(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def lane_top_genes(self, box):
     return _thousands_formatter(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_top_transcripts(self, box):
     return _thousands_formatter(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_top_transcripts(self, box):
     return _thousands_formatter(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def lane_top_transcripts(self, box):
     return _thousands_formatter(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def experiment_top_exons(self, box):
     return _thousands_formatter(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def run_top_exons(self, box):
     return _thousands_formatter(self, box)
 
 
-@augment_resource((JSON, PICKLED))
+@augment((JSON, PICKLED))
 def lane_top_exons(self, box):
     return _thousands_formatter(self, box)
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def experiment_exon_inclusion_profile(self, box):
     return _exon_inclusion_profile(self, box)
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def run_exon_inclusion_profile(self, box):
     return _exon_inclusion_profile(self, box)
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def lane_exon_inclusion_profile(self, box):
     return _exon_inclusion_profile(self, box)
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def experiment_reads_supporting_exon_inclusions(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def run_reads_supporting_exon_inclusions(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def lane_reads_supporting_exon_inclusions(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def experiment_novel_junctions_from_annotated_exons(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def run_novel_junctions_from_annotated_exons(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def lane_novel_junctions_from_annotated_exons(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def experiment_novel_junctions_from_unannotated_exons(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def run_novel_junctions_from_unannotated_exons(self, box):
     pass
 
 
-@augment_resource((JSON,))
+@augment((JSON,))
 def lane_novel_junctions_from_unannotated_exons(self, box):
     pass
 
@@ -680,54 +680,48 @@ def _gene_expression_profile(self, box):
 
 
 def _custom_spaced_chart(self, box):
-    # Need to extract some infos from the table, so load the pickled dictionary
     table = box[PICKLED]
     height = max(len(table['table_data']) * 40, 100)
     box['chartoptions']['height'] = str(height)
-    chartArea = '''{left:"20%", right:"20%", top:"5%",width:"60%",height:"60%"}'''
-    box['chartoptions']['chartArea'] = chartArea
+    area = '''{left:"20%", right:"20%", top:"5%",width:"60%",height:"60%"}'''
+    box['chartoptions']['chartArea'] = area
     box['chartoptions']['hAxis'] = '''{minValue:"0"}'''
 
 
 def _thousands_formatter(self, box):
-    # Need to extract some infos from the table, so load the pickled Python code
     table = box[PICKLED]
     i = 0
     for column, column_type in table['table_description']:
         if column_type == 'number':
-            box['javascript'] += """thousandsformatter.format(data, %s);\n""" % i
+            box['javascript'] += "thousandsformatter.format(data, %s);\n" % i
         i = i + 1
     return box
 
 
 def _detected_genes(self, box):
     box['javascript'] = ""
-    # Need to extract some infos from the table, so load the pickled Python code
     table = box[PICKLED]
     # Add formatting for expression values of lanes
     for i in range(1, len(table['table_description'])):
-        box['javascript'] = box['javascript'] + """thousandsformatter.format(data, %s);\n""" % i
+        box['javascript'] += """thousandsformatter.format(data, %s);\n""" % i
     return box
 
 
 def _mapped_reads(self, box):
-    # Need to extract some infos from the table, so load the pickled dictionary
     table = box[PICKLED]
     height = max(len(table['table_data']) * 40, 100)
     box['chartoptions']['height'] = str(height)
-    chartArea = '''{left:"20%", right:"20%", top:"5%",width:"60%",height:"90%"}'''
+    chartArea = '{left:"20%", right:"20%", top:"5%",width:"60%",height:"90%"}'
     box['chartoptions']['chartArea'] = chartArea
 
 
 def _percentage_of_reads_with_ambiguous_bases(self, box):
-    # Need to extract some infos from the table, so load the pickled dictionary
     table = box[PICKLED]
     height = max(len(table['table_data']) * 60, 160)
     box['chartoptions']['height'] = str(height)
 
 
 def _position(self, box):
-    # Need to extract some infos from the table, so load the pickled dictionary
     vAxis = '''{logScale:true}'''
     box['chartoptions']['vAxis'] = vAxis
     box['chartoptions']['width'] = 900
