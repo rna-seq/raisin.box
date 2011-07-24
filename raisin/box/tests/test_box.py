a4dac4b1e5757a7c4f01c36cfc08b00e2c34cfd0
Maik R��der <maikroeder@gmail.com>
2011-07-24 23:30:13 +0000

    =add tests

---
+++ raisin/box/tests/test_box.py
@@ -0,0 +1,37 @@
+import sys
+import unittest
+from raisin.box import boxes
+from raisin.box.config import PICKLED
+
+
+class BoxTest(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def test_thousands_formatter(self):
+        box = {PICKLED: {'table_description': [['Header 1', 'string'],
+                                               ['Header 2', 'number']]},
+                        'javascript': ''}
+        boxes._thousands_formatter(None, box)
+        javascript = 'thousandsformatter.format(data, 1);\n'
+        self.failUnless(box['javascript'] == javascript)
+
+
+# make the test suite.
+def suite():
+    loader = unittest.TestLoader()
+    testsuite = loader.loadTestsFromTestCase(BoxTest)
+    return testsuite
+
+
+# Make the test suite; run the tests.
+def test_main():
+    testsuite = suite()
+    runner = unittest.TextTestRunner(sys.stdout, verbosity=2)
+    runner.run(testsuite)
+
+if __name__ == "__main__":
+    test_main()
