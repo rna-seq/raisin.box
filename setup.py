e018eb66c64ed5343b4a8c0d53c4fd77f8944892
Maik Röder <maikroeder@gmail.com>
2012-09-26 14:38:01 +0200

    =remodeled using tooth.paste

--- setup.py
+++ setup.py
@@ -1,39 +1,50 @@
-from setuptools import setup, find_packages
-import sys, os
+from setuptools import setup
+from setuptools import find_packages
 
-version = '1.3'
-long_description = """The raisin.box package is a part of Raisin, the web application
-used for publishing the summary statistics of Grape, a pipeline used for processing and
-analyzing RNA-Seq data."""
+
+install_requires = [
+    'setuptools',
+    # -*- Extra requirements: -*-
+    'configobj',
+]
+
+entry_points = """
+    # -*- Entry points: -*-
+    """
+
+classifiers = [
+    'Programming Language :: Python',
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Natural Language :: English',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Operating System :: POSIX :: Linux',
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+]
+
+with open("README.txt") as f:
+    README = f.read()
+
+with open("CHANGES.txt") as f:
+    CHANGES = f.read()
 
 setup(name='raisin.box',
-      version=version,
-      description="A package used in the Raisin web application",
-      long_description=long_description,
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Programming Language :: Python',
-          'Intended Audience :: Developers',
-          'Operating System :: OS Independent',
-          'License :: OSI Approved :: GNU General Public License (GPL)',
-          'Natural Language :: English',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Operating System :: POSIX :: Linux'],
-      keywords='RNA-Seq pipeline ngs transcriptome bioinformatics ETL',
+      version='1.4',
+      packages=find_packages(),
+      description=("A package used in the Raisin web application"),
+      long_description=README + '\n' + CHANGES,
       author='Maik Roder',
       author_email='maikroeder@gmail.com',
-      url='http://big.crg.cat/services/grape',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      namespace_packages = ['raisin'],
-      package_data = {'raisin.box':['*.ini']},
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-          'configobj',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      classifiers=classifiers,
+      install_requires=install_requires,
+      keywords='RNA-Seq pipeline ngs transcriptome bioinformatics ETL',
+      url='http://big.crg.cat/services/grape',
+      license='gpl',
+      namespace_packages=['raisin'],
+      entry_points=entry_points,
       )
